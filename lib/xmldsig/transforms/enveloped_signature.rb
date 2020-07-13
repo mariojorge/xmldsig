dc6bd65c18eb2700e2654ed24008d64f2449c6a9
Mário Jorge <mariojorgenet@gmail.com>
2020-07-13 11:52:10 -0300

    validar quando array vazia

--- lib/xmldsig/transforms/enveloped_signature.rb
+++ lib/xmldsig/transforms/enveloped_signature.rb
@@ -2,8 +2,8 @@ module Xmldsig
   class Transforms < Array
     class EnvelopedSignature < Transform
       def transform
-        byebug
-        signatures = node.xpath("descendant::ds:Signature", Xmldsig::NAMESPACES).sort { |left, right| left.ancestors.size <=> right.ancestors.size }
+        signatures = node.xpath("descendant::ds:Signature", Xmldsig::NAMESPACES).
+            sort { |left, right| left.ancestors.size <=> right.ancestors.size }
 
         signatures.first.remove if signatures.size > 0
         node
